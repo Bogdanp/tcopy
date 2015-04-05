2e6d7cc48b3a5e15bd4031c89397fcab3e235a1e
Bogdan Popa <popa.bogdanp@gmail.com>
2015-04-05 23:09:36 +0300

    check returned values in tests

--- tests/test_tco.py
+++ tests/test_tco.py
@@ -3,6 +3,21 @@ import pytest
 from tcopy import tco
 
 
+def fib_(n):
+    x, y = 1, 1
+    while n > 0:
+        n, x, y = n - 1, y, x + y
+    return x
+
+
+def fact_(n):
+    k = 1
+    while n > 0:
+        k *= n
+        n -= 1
+    return k
+
+
 def fib(n, x=1, y=1):
     if n == 0:
         return x
@@ -15,7 +30,7 @@ def test_fib_1000_no_tco():
 
 
 def test_fib_1000_with_tco():
-    assert tco(fib)(1000)
+    assert tco(fib)(1000) == fib_(1000)
 
 
 def test_nested():
@@ -28,7 +43,7 @@ def test_nested():
 
         return fact
 
-    assert f()(1000)
+    assert f()(1000) == fact_(1000)
 
 
 def test_closures():
@@ -43,4 +58,4 @@ def test_closures():
 
         return fact
 
-    assert f()(1000)
+    assert f()(1000) == fact_(1000)
