f8ab4dc77dec30276ee28accbd20ab5231389626
Bogdan Popa <popa.bogdanp@gmail.com>
2015-04-06 12:49:29 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
 from setuptools import setup
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
     name="tcopy",
-    version="0.1.0",
     packages=["tcopy"],
+    version="0.1.0",
+    description="Basic tail call removal decorator",
+    long_description=long_description,
+    author="Bogdan Popa",
+    author_email="popa.bogdanp@gmail.com",
+    url="https://github.com/Bogdanp/tcopy",
+    keywords=["dangerous", "optimization", "useless"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Code Generators",
+        "Topic :: Software Development :: Interpreters",
+    ],
 )
